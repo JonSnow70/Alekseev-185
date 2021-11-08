61abcfddb7536decd2e16763a48e240be08cb094
vlad <vladislav27.022@gmail.com>
2021-11-08 11:35:40 +0300

    213

---
+++ пароль/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(validatePassword("ASDqwe123$"));
+        }
+        public static bool validatePassword(string password)
+        {
+            if (password.Length < 8 || password.Length > 20)
+                return false;
+            if (!password.Any(Char.IsLower))
+                return false;
+            if (!password.Any(Char.IsUpper))
+                return false;
+            if (!password.Any(Char.IsDigit))
+                return false;
+            if (password.Intersect("#$%^&_").Count() == 0)
+                return false;
+            return true;
+        }
+    }
+}
